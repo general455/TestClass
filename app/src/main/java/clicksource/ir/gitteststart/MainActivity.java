97b710a83b212a6a5157deb64919c5438ad0994e
mojtaba nekooei <mojtabanekooei@gmail.com>
2019-02-17 19:43:34 +0330

    main commit

--- app/src/main/java/clicksource/ir/gitteststart/MainActivity.java
+++ app/src/main/java/clicksource/ir/gitteststart/MainActivity.java
@@ -1,11 +1,12 @@
 package clicksource.ir.gitteststart;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     String test;
+    String newATset;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

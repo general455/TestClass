e7d2b9fa42b9ba89c0dfc6954e0126fc1caf1d26
mojtaba nekooei <mojtabanekooei@gmail.com>
2019-02-17 19:25:44 +0330

    first commit

---
+++ app/src/main/java/clicksource/ir/gitteststart/MainActivity.java
@@ -0,0 +1,14 @@
+package clicksource.ir.gitteststart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    String test;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
